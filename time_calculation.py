7e88adc230f5ff4fae834d8b41e4d31442adc4a4
QuanchenZou <zouquanchen@126.com>
2018-03-06 17:21:09 +0800

    Update time_calculation.py

--- time_calculation.py
+++ time_calculation.py
@@ -1,7 +1,6 @@
 import datetime
 import os
 '''
-# 写之前，先检验文件是否存在，存在就删掉
 if os.path.exists("dest.txt"):
     os.remove("dest.txt")
 '''
@@ -12,10 +11,10 @@ for line in f.readlines():
     #print(line)
     d1 = line[0]
     d2 = line[1]
-    name = line[2]
+    program_name = line[2]
     dd1 = datetime.datetime.strptime(d1, '%Y-%m-%d %H:%M:%S')
     dd2 = datetime.datetime.strptime(d2, '%Y-%m-%d %H:%M:%S')
-    print(name, ":", dd2 - dd1)
+    print(program_name, ":", dd2 - dd1)
 
 '''
     duration = dd2-dd1
